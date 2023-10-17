2a40a8cc44fc91600ed6135e017d6f85eb49daea
Priyanshu085 <abpriyanshu007@gmail.com>
2023-10-17 20:36:40 +0530

    ADD CarCard

--- app/page.tsx
+++ app/page.tsx
@@ -1,11 +1,16 @@
-import Image from 'next/image'
-import { Hero } from '@/components'
+import { CarCard, CustomFilter, Hero } from '@/components'
 import SearchBar from '@/components/SearchBar'
-import CustomFilter from '@/components/CustomFilter'
+import { fetchCars } from '@/utils'
 
-export default function Home() {
+export default async function Home() {
+
+  const allCars = await fetchCars();
+  // console.log(allCars);
+
+  const isDataEmpty = !Array.isArray(allCars) || allCars.length < 1 || !allCars;
+
   return (
-    <main>
+    <main className='overflow-hidden'>
       <Hero />
 
       <div className='mt-12 padding-x paddinng-y max-width' id='discover'>
@@ -23,8 +28,24 @@ export default function Home() {
           </div>
         </div>
 
-      </div>
+        {!isDataEmpty ? (
+          <section>
+          <div className='home__cars-wrapper'>
+            {allCars?.map((car) => (
+              <CarCard car={car} />
+            ))}
+          </div>
 
+        </section>
+      ) : (
+        <div className='home__error-container'>
+          <h2 className='text-black text-xl font-bold'>Oops, no results found</h2>
+          <p>{allCars?.message}</p>
+        </div>
+        
+        )}
+
+      </div>
     </main>
   )
 }
