5adadedd4a5cf2e6de66449374d500156d002cc6
Priyanshu085 <abpriyanshu007@gmail.com>
2023-10-19 20:18:21 +0530

    ADD Dark Mode

--- components/Hero.tsx
+++ components/Hero.tsx
@@ -14,13 +14,13 @@ const Hero = () => {
   };
 
   return (
-    <div className="hero">
+    <div className="hero text-white">
       <div className="flex-1 pt-36 padding-x text-primary">
         <h1 className="hero__title">
           Find, book, rent a car—quick and super easy!
         </h1>
 
-        <p className="hero__subtitle">
+        <p className="hero__subtitle text-white">
           Streamline your car rental experience with our effortless booking
           process.
         </p>
